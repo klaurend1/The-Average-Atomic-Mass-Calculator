74d9ed7906972adbad64486c95515db1d157a039
klaurend1 <153146330+klaurend1@users.noreply.github.com>
2024-02-10 13:36:34 -0600

    Add files via upload
    Description:
    
    This Python script serves as an Atomic Mass Calculator, allowing users to input information about three isotopes, including their atomic masses and abundances. The script then efficiently calculates and displays the average atomic mass based on the provided data. This tool is designed for educational purposes, aiding students and enthusiasts in understanding and computing average atomic masses.
    
    Features:
    
    Simple and user-friendly interface for entering isotopes and abundances.
    Efficient algorithm for calculating the average atomic mass.
    Educational tool for learning about atomic masses and their significance.
    
    Usage:
    
    Run the script.
    Input the atomic masses of three isotopes when prompted.
    Input the corresponding abundances in decimal form.
    The script will calculate and display the average atomic mass.

---
+++ Average Atomic Mass Calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Enter 3 isotopes here: ")
+isotope1 = float(input("Isotope 1: "))
+isotope2 = float(input("Isotope 2: "))
+isotope3 = float(input("Isotope 3: "))
+
+print("Enter abundance in decimal form for each isotope below: ")
+abundance1 = float(input("Abundance 1: "))
+abundance2 = float(input("Abundance 2: "))
+abundance3 = float(input("Abundance 3: "))
+
+atomicmass = float(abundance1) * float(isotope1) + float(abundance2) * float(isotope2) + float(abundance3) * float(isotope3)
+print("The Average Atomic Mass is...", atomicmass)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
